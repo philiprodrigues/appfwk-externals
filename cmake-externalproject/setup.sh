2ac81a37a35e5877505d72e0d2c9f029f6cc53ec
Philip Rodrigues <rodriges@fnal.gov>
2020-06-11 11:16:34 +0100

    Add code for building the application framework externals using cmake's ExternalProject, not as UPS products

---
+++ cmake-externalproject/setup.sh
@@ -0,0 +1,9 @@
+source /cvmfs/fermilab.opensciencegrid.org/products/artdaq/setup
+PRODUCTS=/cvmfs/fermilab.opensciencegrid.org/products/artdaq:/cvmfs/fermilab.opensciencegrid.org/products/larsoft:/cvmfs/fermilab.opensciencegrid.org/products/larsoft
+setup cmake     v3_14_3
+setup gcc       v8_2_0
+setup TRACE     v3_15_07
+setup boost     v1_70_0    -q e19:prof
+setup ninja     v1_8_2
+setup cetlib    v3_10_00   -q e19:prof
+
