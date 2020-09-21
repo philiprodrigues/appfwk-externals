69a3584da2e736ce58046fc7247e4d8d4a93953a
Brett Viren <brett.viren@gmail.com>
2020-09-21 12:09:02 -0400

    Fix appfwk 1.1.0 inclusion of daq-buildtools

--- dunedaq-spack-repo/packages/appfwk/package.py
+++ dunedaq-spack-repo/packages/appfwk/package.py
@@ -36,6 +36,9 @@ class Appfwk(CMakePackage):
     version('1.1.0', sha256='3a88d45b5251748d0041fb7ad3df07c086d5acf3c27b1ca213887c3ad15bee98')
     version('1.0.0', sha256='2d61dd2d9b685351f840cf085606935960ab2e437ff10be14be1d67adeea0255')
 
+    # probably needed earlier than 1.1.0
+    patch("fix-find-dqt.patch", when="1.1.0")
+
     # FIXME: Add dependencies if required.
     depends_on('daq-buildtools@spack-build')
     depends_on('boost')
